fd40601fc8e320b52c0094598a7fd56fa297a7d9
Jean-Tiare Le Bigot <jtlebi@gmail.com>
2011-09-17 17:43:20 +0200

    removed some local config bits for pull request :)

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ var config = {
   allow_ip_list: './config/allow_ip_list',
   black_list:    './config/black_list',
   host_filters:   './config/hostfilters.js',
-  listen:[{ip:'91.121.154.113', port:80},
-          {ip:'2001:41d0:1:db71::1', port:80}]    
+  listen:[{ip:'0.0.0.0', port:80},//all ipv4 interfaces
+          {ip:'::', port:80}]//all ipv6 interfaces
 };
 
 exports.config = config;
