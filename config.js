8ab62cdf044f61455a0e9d554f32e5bdcb522468
Jean-Tiare Le Bigot <jtlebi@gmail.com>
2012-02-05 15:21:03 +0100

    removed my server specifig config :)

--- config.js
+++ config.js
@@ -7,12 +7,12 @@
 */
 
 var config = {
-  add_proxy_header: true,
+  add_proxy_header: true,//activate addition of X-Forwarded-For header for better logging on real server side
   allow_ip_list: './config/allow_ip_list',
   black_list:    './config/black_list',
   host_filters:   './config/hostfilters.js',
-  listen:[{ip:'91.121.154.113', port:80},
-          {ip:'2001:41d0:1:db71::1', port:80}]    
+  listen:[{ip:'0.0.0.0', port:80},//all ipv4 interfaces
+          {ip:'::', port:80}]//all ipv6 interfaces
 };
 
 exports.config = config;
