a7ba47649981d06b9a3649d717da0454773a9883
Jean-Tiare Le Bigot <jtlebi@gmail.com>
2012-03-11 21:57:46 +0100

    added httpS support

--- config.js
+++ config.js
@@ -6,6 +6,8 @@
 **
 */
 
+var fs = require('fs');
+
 var config = {
   add_proxy_header: true,//activate addition of X-Forwarded-For header for better logging on real server side
   allow_ip_list: './config/allow_ip_list',
@@ -13,6 +15,23 @@ var config = {
   host_filters:   './config/hostfilters.js',
   listen:[{ip:'0.0.0.0', port:80},//all ipv4 interfaces
           {ip:'::', port:80}]//all ipv6 interfaces
+  listen_ssl:[{
+              ip:'0.0.0.0',//all *secure* ipv4 interfaces
+              port:443,
+              key:fs.readFileSync('/path/to/ssl.key'),
+              cert:fs.readFileSync('/path/to/ssl.crt'),
+              ca:[fs.readFileSync('/path/to/ca.pem'), 
+                  fs.readFileSync('/path/to/sub-ca.pem')]
+            },{ 
+              ip:'::',//all *secure* ipv6 interfaces
+              port:443,
+              key:fs.readFileSync('/path/to/ssl.key'),
+              cert:fs.readFileSync('/path/to/ssl.crt'),
+              ca:[fs.readFileSync('/path/to/ca.pem'), 
+                  fs.readFileSync('/path/to/sub-ca.pem')]
+
+            }   
+           ]
 };
 
 exports.config = config;
